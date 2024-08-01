4550245d06e151df8167008519a53c48b3717283
Varshneyanurag125 <22bee031@nith.ac.in>
2024-08-01 16:42:23 +0530

    add speaker

---
+++ speaker.js
@@ -0,0 +1,12 @@
+const cards = document.querySelectorAll('.card');
+let currentIndex = 1; // Start with the middle card
+
+function rotateCards() {
+    currentIndex = (currentIndex + 1) % 3;
+    cards.forEach((card, index) => {
+        const position = (index - currentIndex + 3) % 3;
+        card.className = 'card ' + ['left', 'center', 'right'][position];
+    });
+}
+
+setInterval(rotateCards, 3000);
